dae2726647d244b3f9b578c1827166c9e1cec562
Russell Gordon <rgordon@lcs.on.ca>
2021-02-11 20:58:59 -0500

    Added iOS target.

---
+++ iOS-DataStorageConverter/iOS_DataStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataStorageConverterApp.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Russell Gordon on 2021-02-11.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
